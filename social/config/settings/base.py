cae04a8a1941defa3ea7cdc2e8ebbe368a58e94c
Winstein Caldeira Martins <sriwinstein@gmail.com>
2016-06-17 02:12:26 -0300

    Primeiro commit

---
+++ social/config/settings/base.py
@@ -0,0 +1,128 @@
+# -*- conding: utf-8 -*-
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+APPS_DIR = BASE_DIR + '/social'
+
+
+# APP CONFIGURATION
+# ------------------------------------------------------------------------------
+DJANGO_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+THIRD_PARTY_APPS = [
+]
+
+LOCAL_APPS = [
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+
+# MIDDLEWARE CONFIGURATION
+# ------------------------------------------------------------------------------
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+
+# GENERAL CONFIGURATION
+# ------------------------------------------------------------------------------
+LANGUAGE_CODE = 'pt-br'
+
+TIME_ZONE = 'america/sao_paulo'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# TEMPLATE CONFIGURATION
+# ------------------------------------------------------------------------------
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            APPS_DIR + '/templates'
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+# STATIC FILE CONFIGURATION
+# ------------------------------------------------------------------------------
+# STATIC_ROOT = str(ROOT_DIR('staticfiles'))
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    APPS_DIR + '/static',
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+
+# MEDIA CONFIGURATION
+# ------------------------------------------------------------------------------
+MEDIA_ROOT = APPS_DIR + '/media'
+
+MEDIA_URL = '/media/'
+
+
+# URL CONFIGURATION
+# ------------------------------------------------------------------------------
+ROOT_URLCONF = 'config.urls'
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+
+# AUTH CONFIGURATION
+# ------------------------------------------------------------------------------
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation' +
+        '.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation' +
+        '.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation' +
+        '.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation' +
+        '.NumericPasswordValidator',
+    },
+]
+
+ALLOWED_HOSTS = []
