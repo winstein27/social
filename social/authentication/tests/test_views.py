12a802f588eee79146dcb82d784e21d033d6358f
Winstein Caldeira Martins <sriwinstein@gmail.com>
2016-06-25 01:31:04 -0300

    Adding tests ProfilePasswordView Adding action to profile form

--- social/authentication/tests/test_views.py
+++ social/authentication/tests/test_views.py
@@ -2,8 +2,9 @@
 
 from django.core.urlresolvers import reverse
 
-from django.test import TestCase
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.test import TestCase
 
 import os
 
@@ -188,3 +189,78 @@ class ProfileViewTest(TestCase):
         self.assertContains(response, 'new@email.com')
         self.assertContains(response, 'changed_first_name')
         self.assertContains(response, 'changed_last_name')
+
+
+class ProfilePasswordViewTest(TestCase):
+
+    username = 'test_user'
+    password = 'user_password'
+
+    @classmethod
+    def setUp(cls):
+        cls.user = User()
+        cls.user.username = cls.username
+        cls.user.set_password(cls.password)
+        cls.user.save()
+
+    @classmethod
+    def tearDown(cls):
+        cls.user.delete()
+
+    def try_to_change_password(self, context):
+        self.client.login(username=self.username, password=self.password)
+        return self.client.post(reverse('authentication:password'), context)
+
+    def test_change_password_with_wrong_old_password(self):
+        """
+        Trying to change the password with a wrong old password must not chage
+        it
+        """
+        context = {
+            'old_password': 'wrong_password',
+            'new_password': 'new_password',
+            'verification_password': 'new_password'
+        }
+
+        response = self.try_to_change_password(context)
+
+        self.assertEqual(response.status_code, 409)
+        self.assertNotEqual(
+            authenticate(username=self.username, password=self.password), None)
+
+    def test_change_password_with_different_new_and_verification_passwords(
+            self):
+        """
+        Trying to change the password with different new and verification
+        password must not change it
+        """
+        context = {
+            'old_password': self.password,
+            'new_password': 'new_password',
+            'verification_password': 'verification_password'
+        }
+
+        response = self.try_to_change_password(context)
+
+        self.assertEqual(response.status_code, 409)
+        self.assertNotEqual(
+            authenticate(username=self.username, password=self.password), None)
+
+    def test_change_password_with_correct_values(self):
+        """
+        Trying to change the password with correct old password and same new
+        and verification password must change the user's password
+        """
+        new_password = 'new_password'
+        context = {
+            'old_password': self.password,
+            'new_password': new_password,
+            'verification_password': new_password
+        }
+
+        response = self.try_to_change_password(context)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(
+            authenticate(username=self.username, password=new_password),
+            None)
